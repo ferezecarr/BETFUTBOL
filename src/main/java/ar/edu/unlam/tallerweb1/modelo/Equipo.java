c680467c56416359c456d37e685b4a2da2d6777d
jmartingimenez <jmartingimenez@hotmail.com>
2017-10-25 19:06:55 -0300

    Estableciendo las relaciones entre Equipo y Partido

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Equipo.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Equipo.java
@@ -1,3 +1,4 @@
+/*HashCode()/Equals() implementando para distinguir entre nombres*/
 package ar.edu.unlam.tallerweb1.modelo;
 
 import javax.persistence.Entity;
@@ -11,6 +12,47 @@ public class Equipo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nombre;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipo other = (Equipo) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Partido.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Partido.java
@@ -1,56 +1,114 @@
+/*HashCode()/Equals() implementando para distinguir entre id, local y visitante a la vez. 
+ * Hay una doble relacion entre Equipo y Partido...
+ * 		Equipo(1) 	-(Hace de local en)- 		(N)Partido
+ * 		Equipo(1) 	-(Hace de visitante en)- 	(N)Partido*/
 package ar.edu.unlam.tallerweb1.modelo;
 
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Partido {
-	
+public class Partido {	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String rival;
-	private Date fecha;
-	private Integer resultado;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_equipo_local")
+	private Equipo local;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_equipo_visitante")
+	private Equipo visitante;	
+	
+	private Date fecha;
+	private Integer golesLocal = 0;
+	private Integer golesVisitante = 0;
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	public String getRival() {
-		return rival;
+
+	public Equipo getLocal() {
+		return local;
 	}
-	
-	public void setRival(String rival) {
-		this.rival = rival;
+
+	public void setLocal(Equipo local) {
+		this.local = local;
 	}
-	
+
+	public Equipo getVisitante() {
+		return visitante;
+	}
+
+	public void setVisitante(Equipo visitante) {
+		this.visitante = visitante;
+	}
+
 	public Date getFecha() {
 		return fecha;
 	}
-	
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-	
-	public Integer getResultado() {
-		return resultado;
+
+	public Integer getGolesLocal() {
+		return golesLocal;
+	}
+
+	public void setGolesLocal(Integer golesLocal) {
+		this.golesLocal = golesLocal;
+	}
+
+	public Integer getGolesVisitante() {
+		return golesVisitante;
+	}
+
+	public void setGolesVisitante(Integer golesVisitante) {
+		this.golesVisitante = golesVisitante;
 	}
 	
-	public void setResultado(Integer resultado) {
-		this.resultado = resultado;
+	public String mostrarResultado(){
+		return local.getNombre() + " " + golesLocal + " - " + golesVisitante + 
+				" " + visitante.getNombre();
 	}
-	
-	
-	
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Partido other = (Partido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (local == null) {
+			if (other.local != null)
+				return false;
+		} else if (!local.equals(other.local))
+			return false;
+		if (visitante == null) {
+			if (other.visitante != null)
+				return false;
+		} else if (!visitante.equals(other.visitante))
+			return false;
+		return true;
+	}	
 }
