15a4386cf1d41e7f50afc73c4c29f2f3e1ec2bbb
Fernando <ferezecarr@gmail.com>
2017-10-27 09:57:49 -0300

    Agregando la conexion a la base de datos

---
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -0,0 +1,20 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.*;
+import ar.edu.unlam.tallerweb1.SpringTest;
+
+//Clase que prueba la conexion a la base de datos y hereda de SpringTest por lo que corre dentro del contexto
+//de spring
+public class ConexionBaseDeDatosTest extends SpringTest {
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void pruebaConexion() {
+		assertThat(getSession().isConnected()).isTrue();
+	}
+
+}
