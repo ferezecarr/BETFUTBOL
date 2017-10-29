b0df04b14d0ff4198d3e8a5cb048b9a04da15111
jmartingimenez <jmartingimenez@hotmail.com>
2017-10-28 21:10:38 -0300

    Asserts de ejemplo - Apuesta (EN PROGRESO)

--- src/test/java/ar/edu/unlam/tallerweb1/modelo/ApuestaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/ApuestaTest.java
@@ -1,5 +1,9 @@
+/*NOTA: En relaciones bidireccionales, aparentemente hay que setear los dos lados para 
+ * que quede bien la relacion. Despues el metodo .save, si creo que se puede usar de 
+ * un lado solo (el 'cascade' hace la magia)*/
 package ar.edu.unlam.tallerweb1.modelo;
 
+import java.util.LinkedList;
 import java.util.List;
 import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
@@ -17,7 +21,6 @@ public class ApuestaTest extends SpringTest{
 		//Creando un usuario
 		Usuario pepe = new Usuario();
 		pepe.setNombreYApellido("Pepe Pompin");
-		//pepe.setApuestas(new HashSet<>());
 		
 		//Creando dos equipos de futbol
 		Equipo Boca = new Equipo();
@@ -49,11 +52,27 @@ public class ApuestaTest extends SpringTest{
 		victoriaVisitante.setEvento(evento);
 		empate.setEvento(evento);
 		
+		/*Esto es porque la relacion es bidireccional. Si no lo hago, cuando criteria 
+		 * traiga los objetos, el evento.getCuotas() no va tener nada.
+		 * Es lo mismo con las relaciones 'apuesta-relacion'/'apuesta-usuario'*/
+		List<Cuota> aux1 = new LinkedList<Cuota>();
+		aux1.add(victoriaLocal);
+		aux1.add(victoriaVisitante);
+		aux1.add(empate);
+		evento.setCuotas(aux1);
+		
 		//Creando una apuesta que realiza pepe para el evento denominado superclasico
 		Apuesta apuesta = new Apuesta();
 		apuesta.setApostador(pepe);
 		apuesta.setCantidadApostada(10.00d);
+		apuesta.setCuotaApostada(empate.getValor());
 		apuesta.setEvento(evento);
+		
+		//Por la bidireccionalidad
+		List<Apuesta> aux2 = new LinkedList<Apuesta>();
+		aux2.add(apuesta);
+		pepe.setApuestas(aux2);
+		evento.setApuestas(aux2);
 		
 		//Guardando en la BDD el usuario
 		getSession().save(pepe);
@@ -158,6 +177,15 @@ public class ApuestaTest extends SpringTest{
 		
 		/*Traer usuarios de un determinado nombre que apostaron en un evento de una 
 		 * determinada id*/
+		List<Apuesta> loDeArriba;
+		loDeArriba = getSession().createCriteria(Apuesta.class)
+				.createAlias("apostador", "a")
+				.add(Restrictions.eq("a.nombreYApellido", "Pepe Pompin"))
+				.createAlias("evento", "e")
+				.add(Restrictions.eq("e.id", 1L))
+				.list();
+		assertThat(loDeArriba.get(0).getApostador().getNombreYApellido()).isEqualTo("Pepe Pompin");
+		assertThat(loDeArriba.get(0).getEvento().getId()).isEqualTo(1L);
 		
 		/*Traer los usuarios que realizaron apuestas en un evento de tipo resultado, donde 
 		 * juega Boca de local realizadas por un determinado usuario, en el que existe una 
