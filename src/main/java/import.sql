23759899ebd8e6cddf014c70779512d7f52c1c4c
jmartingimenez <jmartingimenez@hotmail.com>
2017-11-20 21:14:56 -0300

    Hardcodeando cosas en el script sql - (EN PROGRESO)

--- src/main/java/import.sql
+++ src/main/java/import.sql
@@ -10,36 +10,40 @@ INSERT INTO Usuario (id, nombreYApellido, email, password, rol) VALUES (2,'Ferna
 -- Creando diez equipos
 INSERT INTO Equipo (id, nombre) VALUES (1, "Argentina");
 INSERT INTO Equipo (id, nombre) VALUES (2, "China");
-INSERT INTO Equipo (id, nombre) VALUES (3, "Inglaterra");
-INSERT INTO Equipo (id, nombre) VALUES (4, "Corea del sur");
-INSERT INTO Equipo (id, nombre) VALUES (5, "Belgica");
-INSERT INTO Equipo (id, nombre) VALUES (6, "Uruguay");
-INSERT INTO Equipo (id, nombre) VALUES (7, "Nigeria");
-INSERT INTO Equipo (id, nombre) VALUES (8, "Alemania");
-INSERT INTO Equipo (id, nombre) VALUES (9, "Jamaica");
-INSERT INTO Equipo (id, nombre) VALUES (10, "Brasil");
+INSERT INTO Equipo (id, nombre) VALUES (3, "Napoli");
+INSERT INTO Equipo (id, nombre) VALUES (4, "Juventus");
+INSERT INTO Equipo (id, nombre) VALUES (5, "Barcelona");
+INSERT INTO Equipo (id, nombre) VALUES (6, "Celta de Vigo");
+INSERT INTO Equipo (id, nombre) VALUES (7, "Racing de Estraburgo");
+INSERT INTO Equipo (id, nombre) VALUES (8, "PSG");
+INSERT INTO Equipo (id, nombre) VALUES (9, "Independiente");
+INSERT INTO Equipo (id, nombre) VALUES (10, "Rosario Central");
+INSERT INTO Equipo (id, nombre) VALUES (11, "Gimnasia LP");
+INSERT INTO Equipo (id, nombre) VALUES (12, "River Plate");
 
--- Creando seis partidos
+-- Creando siete partidos
 INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (1,2,1,'2017-11-01 15:30:00', 0, 4, TRUE, TRUE);
 INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (2,1,2,'2017-11-15 13:00:00', 0, 0, TRUE, FALSE);
-INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (3,3,4,'2017-11-29 13:00:00', 0, 0, FALSE, FALSE);
-INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (4,5,6,'2017-11-29 16:00:00', 0, 0, FALSE, FALSE);
-INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (5,7,8,'2017-11-29 16:00:00', 0, 0, FALSE, FALSE);
-INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (6,9,10,'2017-11-29 18:30:00', 0, 0, FALSE, FALSE);
+INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (3,3,4,'2017-12-01 16:45:00', 0, 0, FALSE, FALSE);
+INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (4,5,6,'2017-12-02 09:00:00', 0, 0, FALSE, FALSE);
+INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (5,7,8,'2017-12-02 13:00:00', 0, 0, FALSE, FALSE);
+INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (6,9,10,'2017-12-02 19:15:00', 0, 0, FALSE, FALSE);
+INSERT INTO Partido (id, id_equipo_local, id_equipo_visitante, fecha, golesLocal, golesVisitante, isTerminado, isResultadoFinal) VALUES (7,11,12,'2017-12-03 21:30:00', 0, 0, FALSE, FALSE);
 
--- Creando seis eventos de tipo "Resultado" (Gana, empate, derrota)
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado, cuotaGanadora) VALUES (1, 1, "Resultado", "| China Vs Argentina | 01/11 - 13:00hs", TRUE, "Gana Argentina");
+-- Creando siete eventos de tipo "Resultado" (Gana, empate, derrota)
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado, cuotaGanadora) VALUES (1, 1, "Resultado", "| China Vs Argentina | 01/11 - 15:30hs", TRUE, "Gana Argentina");
 INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (2, 2, "Resultado", "| Argentina Vs China | 29/11 - 13:00hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (3, 3, "Resultado", "| Inglaterra Vs Corea del Sur | 29/11 - 13:00hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (4, 4, "Resultado", "| Belgica Vs Uruguay | 29/11 - 16:00hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (5, 5, "Resultado", "| Nigeria Vs Alemania | 29/11 - 16:00hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (6, 6, "Resultado", "| Jamaica Vs Brasil | 29/11 - 18:30hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (3, 3, "Resultado", "| Serie A | 01/12 - 16:45hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (4, 4, "Resultado", "| Liga BBVA | 02/12 - 09:00hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (5, 5, "Resultado", "| Ligue One | 02/12 - 13:00hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (6, 6, "Resultado", "| Primera División Argentina | 02/12 - 19:15hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (7, 7, "Resultado", "| Primera División Argentina | 03/12 - 21:30hs", FALSE);
 
 -- Creando cuatro eventos de tipo "Equipo hace goles" (ninguno, uno, dos, mas de dos)
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (7, 5, "Cuantos goles hace un equipo", "| Amistoso internacional - Goles de Nigeria ante Alemania | 21/11 - 18:30hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (8, 5, "Cuantos goles hace un equipo", "| Amistoso internacional - Goles de Alemania ante Nigeria | 21/11 - 18:30hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (9, 6, "Cuantos goles hace un equipo", "| Amistoso internacional - Goles de Jamaica ante Brasil | 21/11 - 18:30hs", FALSE);
-INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (10, 6, "Cuantos goles hace un equipo", "| Amistoso internacional - Goles de Brasil ante Jamaica | 21/11 - 18:30hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (8, 5, "Cuantos goles hace un equipo", "| Ligue One | Goles de Racing de Estraburgo ante PSG | 02/12 - 13:00hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (9, 5, "Cuantos goles hace un equipo", "| Ligue One | Goles de PSG ante Racing de Estraburgo | 02/12 - 13:00hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (10, 6, "Cuantos goles hace un equipo", "| Primera División Argentina | Goles de Independiente ante Rosario Central | 02/12 - 19:15hs", FALSE);
+INSERT INTO Evento (id, id_partido, nombre, descripcion, isTerminado) VALUES (11, 6, "Cuantos goles hace un equipo", "| Primera División Argentina | Goles de Rosario Central ante Independiente | 02/12 - 19:15hs", FALSE);
 
 -- Creando cuotas que son asignadas a los eventos de tipo "Resultado"
 INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (1, 1, "Gana China", 4.90, 0);
@@ -50,42 +54,46 @@ INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (4, 2, "G
 INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (5, 2, "Empate", 2.32, 0);
 INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (6, 2, "Gana China", 4.90, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (7, 3, "Gana Inglaterra", 1.89, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (8, 3, "Empate", 2.5, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (9, 3, "Gana Corea del Sur", 2.5, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (7, 3, "Gana Napoli", 1.89, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (8, 3, "Empate", 2.08, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (9, 3, "Gana Juventus", 2.5, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (10, 4, "Gana Belgica", 2.01, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (10, 4, "Gana Barcelona", 1.55, 0);
 INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (11, 4, "Empate", 2.93, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (12, 4, "Gana Uruguay", 2.44, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (12, 4, "Gana Celta de Vigo", 3.78, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (13, 5, "Gana Nigeria", 2.51, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (14, 5, "Empate", 2.28, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (15, 5, "Gana Alemania", 1.94, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (13, 5, "Gana Racing de Estraburgo", 5.03, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (14, 5, "Empate", 2.76, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (15, 5, "Gana PSG", 1.48, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (16, 6, "Gana Jamaica", 3.01, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (17, 6, "Empate", 2.23, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (18, 6, "Gana Brasil", 1.67, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (16, 6, "Gana Independiente", 1.79, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (17, 6, "Empate", 2.36, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (18, 6, "Gana Rosario Central", 2.43, 0);
+
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (19, 7, "Gana Gimnasia LP", 2.06, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (20, 7, "Empate", 1.64, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (21, 7, "Gana River Plate", 1.87, 0);
 
 -- Creando cuotas que son asignadas a los eventos de tipo "Equipo hace goles"
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (19, 7, "0", 1.43, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (20, 7, "1", 1.99, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (21, 7, "2", 2.50, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (22, 7, "+2", 3.01, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (22, 8, "0", 1.43, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (23, 8, "1", 1.99, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (24, 8, "2", 2.50, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (25, 8, "+2", 3.01, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (23, 8, "0", 3.01, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (24, 8, "1", 2.50, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (25, 8, "2", 1.99, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (26, 8, "+2", 1.43, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (26, 9, "0", 3.01, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (27, 9, "1", 2.50, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (28, 9, "2", 1.99, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (29, 9, "+2", 1.43, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (27, 9, "0", 1.38, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (28, 9, "1", 1.77, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (29, 9, "2", 2.63, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (30, 9, "+2", 5.08, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (30, 10, "0", 2.00, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (31, 10, "1", 1.90, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (32, 10, "2", 2.13, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (33, 10, "+2", 2.26, 0);
 
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (31, 10, "0", 5.08, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (32, 10, "1", 2.63, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (33, 10, "2", 1.77, 0);
-INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (34, 10, "+2", 1.38, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (34, 11, "0", 1.70, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (35, 11, "1", 2.00, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (36, 11, "2", 2.65, 0);
+INSERT INTO Cuota (id, evento_id, nombre, valor, cantidadVotos) VALUES (37, 11, "+2", 3.43, 0);
 
 -- Hardcodeando apuestas
 INSERT INTO Apuesta(id, evento_id, apostador_id, cantidadApostada, cuotaValor, cuotaNombre, isGanadora) VALUES (1, 1, 2, 20.50, 1.44, "Gana Argentina", TRUE);
