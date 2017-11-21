29b7d458d0db0aa601dea3de2ee75e7278e0caad
Fernando <ferezecarr@gmail.com>
2017-11-21 16:47:29 -0300

    update

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorABMEquipos.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorABMEquipos.java
@@ -1,18 +1,34 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Equipo;
+import ar.edu.unlam.tallerweb1.servicios.ServicioEquipo;
+
 @Controller
 public class ControladorABMEquipos {
 	
-	@RequestMapping(path = "añadir-equipo" , method = RequestMethod.POST)
-	public ModelAndView añadirEquipo() {
+	@Inject
+	private ServicioEquipo servicioEquipo;
+	
+	/*@RequestMapping(path = "añadir-equipo" , method = RequestMethod.POST)
+	public ModelAndView añadirEquipo(@ModelAttribute("equipo") Equipo equipo) {
 		ModelMap modelo = new ModelMap();
-		return new ModelAndView();
+		equipo = servicioEquipo.guardarEquipo(equipo);
+		return new ModelAndView("equipos" , modelo);
 	}
+	
+	public ModelAndView actualizarEquipo() {
+		
+	}*/
 
 }
--- src/main/java/ar/edu/unlam/tallerweb1/dao/EquipoDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/EquipoDaoImpl.java
@@ -53,4 +53,5 @@ public class EquipoDaoImpl implements EquipoDao{
 		
 	}
 
+
 }
