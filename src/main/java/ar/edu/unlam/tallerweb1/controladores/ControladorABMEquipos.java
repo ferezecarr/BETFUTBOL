4dae214bfaa6207b3a0d2326160d5268b0b04e39
Fernando <ferezecarr@gmail.com>
2017-11-22 16:49:10 -0300

    Casi terminado el ABM De Equipos

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorABMEquipos.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorABMEquipos.java
@@ -31,21 +31,10 @@ public class ControladorABMEquipos {
 	private ServicioLogin servicioLogin;
 	
 
-	
-	/*@RequestMapping(path = "añadir-equipo" , method = RequestMethod.POST)
-	public ModelAndView añadirEquipo(@ModelAttribute("equipo") Equipo equipo) {
-		ModelMap modelo = new ModelMap();
-		equipo = servicioEquipo.guardarEquipo(equipo);
-		return new ModelAndView("equipos" , modelo);
-	}
-	
-	public ModelAndView actualizarEquipo() {
-		
-	}*/
 
 	
 	@RequestMapping(path = "ABM-Equipo")
-	public ModelAndView añadirEquipo(HttpServletRequest request) {
+	public ModelAndView añadirEquipo(@ModelAttribute("equipo") Equipo equipo , HttpServletRequest request) {
 
 		
 		if(request.getSession().getAttribute("AdminId") == null) {
@@ -62,7 +51,68 @@ public class ControladorABMEquipos {
 		List<Equipo> equipos = servicioEquipo.listarTodosLosEquipos();
 		modelo.put("equipos", equipos);
 		
+		if(servicioEquipo.consultarEquipo(equipo) == null) {
+			servicioEquipo.guardarEquipo(equipo);
+			request.getSession().setAttribute("userAdmin", equipo.getId());
+			modelo.put("equipo", equipo);
+			modelo.put("nombre", equipo.getNombre());
+			modelo.put("aviso-añadir-equipo", "Se añadio el equipo correctamente");
+		} else {
+			modelo.put("aviso-error-añadir-equipo", "No se pudo añadir el equipo");
+		}
+		
 		return new ModelAndView("ABM-Equipo",modelo);
+	}
+	
+	@RequestMapping(path = "actualizar-equipo" , method = RequestMethod.POST)
+	public ModelAndView actualizarEquipo(@ModelAttribute("equipo") Equipo equipo , HttpServletRequest request) {
+		
+		if(request.getSession().getAttribute("AdminId") == null) {
+			return new ModelAndView("redirect:/");
+		}
+		
+		ModelMap modelo = new ModelMap();
+		
+		List<Equipo> equipos = servicioEquipo.listarTodosLosEquipos();
+		modelo.put("equipos", equipos);
+		
+		if(servicioEquipo.consultarEquipo(equipo) != null) {
+			servicioEquipo.actualizarEquipo(equipo);
+			request.getSession().setAttribute("userAdmin", equipo.getId());
+			modelo.put("equipo", equipo);
+			modelo.put("nombre", equipo.getNombre());
+			modelo.put("aviso-actualizar-equipo", "Se actualizo correctamente");
+		} else {
+			modelo.put("aviso-error-actualizar-equipo", "No se pudo actualizar");
+		}
+		
+		return new ModelAndView("ABM-Equipo" , modelo);
+	}
+	
+	@RequestMapping(path = "eliminar-equipo" , method = RequestMethod.POST)
+	public ModelAndView eliminarEquipo(@ModelAttribute("equipo") Equipo equipo , HttpServletRequest request) {
+		
+		if(request.getSession().getAttribute("AdminId") == null) {
+			return new ModelAndView("redirect:/");
+		}
+		
+		ModelMap modelo = new ModelMap();
+		
+		List<Equipo> equipos = servicioEquipo.listarTodosLosEquipos();
+		modelo.put("equipos", equipos);
+		
+		if(servicioEquipo.consultarEquipo(equipo) != null) {
+			servicioEquipo.eliminarEquipo(equipo);
+			request.getSession().setAttribute("userAdmin", equipo.getId());
+			modelo.put("equipo", equipo);
+			modelo.put("nombre", equipo.getNombre());
+			modelo.put("aviso-eliminar-equipo", "Se elimino correctamente");
+		} else {
+			modelo.put("aviso-error-eliminar-equipo", "No se pudo eliminar");
+		}
+		
+		return new ModelAndView("ABM-Equipo" , modelo);
+		
 	}
 	
 	
