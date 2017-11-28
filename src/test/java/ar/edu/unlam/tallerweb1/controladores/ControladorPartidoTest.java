f6bd1b2799d54952baeb5ebe05650cc00768185c
Fernando <ferezecarr@gmail.com>
2017-11-28 18:48:16 -0300

    Agregando un mock para que se finalize el partido

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPartidoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPartidoTest.java
@@ -94,11 +94,26 @@ public class ControladorPartidoTest {
 	}
 	
 	@Test
-	public void testQuePuedEliminarUnPartidoValido() {
+	public void testQuePuedaEliminarUnPartidoValido() {
 		when(request.getSession()).thenReturn(session);
 		when(servicioPartido.consultarPartido(any(Partido.class))).thenReturn(partido);
 		
 		ModelAndView modelo = controladorABMPartido.eliminarPartido(partido, request);
+		
+		assertThat(modelo.getViewName()).isEqualTo("redirect:/");
+		assertThat(modelo.getModel()).isEmpty();
+		
+		verify(session , times(0)).setAttribute("LOCAL", "PARTIDO");
+		verify(session , times(0)).setAttribute("VISITANTE", "PARTIDO");
+		verify(session , times(0)).setAttribute("FECHA", "PARTIDO");
+	}
+	
+	@Test
+	public void testQueSePuedaDarPorTerminadoUnPartido() {
+		when(request.getSession()).thenReturn(session);
+		when(servicioPartido.consultarPartido(any(Partido.class))).thenReturn(partido);
+		
+		ModelAndView modelo = controladorABMPartido.TerminarPArtido(partido, request);
 		
 		assertThat(modelo.getViewName()).isEqualTo("redirect:/");
 		assertThat(modelo.getModel()).isEmpty();
